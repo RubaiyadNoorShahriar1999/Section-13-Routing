c2079fe1f0b944e1fb61ce99c06223ac6dda8f58
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2023-06-25 16:04:32 +0600

    Done till video 179

--- routing-01-starting-setup/routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/routing-01-starting-setup/src/main.js
@@ -5,13 +5,27 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const route = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component: TeamsList },
+    { path: '/', redirect: '/teams' },
+    {
+      name: 'teams',
+      path: '/teams',
+      component: TeamsList,
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ],
+    },
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
 });
